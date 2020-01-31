1886708390a1c0b2f2b96e9d8d28c3a1ece379d9
jarpar <Mamotoja88>
2020-01-31 12:06:40 +0100

    fixed

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/*
+Trójkąt
+Napisać metodę rysującą trójkąt z gwiazdek dla zadanego n. Np, dla n=6
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = n;
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            count = n - i;
+            while (count > 0) {
+                System.out.print("*");
+                count--;
+            }
+        }
+    }
+}
